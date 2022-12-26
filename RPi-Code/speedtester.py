c19b9b739eda5622cf4b33f3862e1ea3b6306a91
Isaac <ibasquerice@gmail.com>
2022-12-26 16:32:51 +0000

    speedtester now sends data to json object

--- RPi-Code/speedtester.py
+++ RPi-Code/speedtester.py
@@ -8,36 +8,69 @@ Date: 24/12/2022
 """
 
 import time
+import json
+import os
+
 import speedtest
+
+def send_to_json(now, ssid, down_speed, up_speed, ping, runtime):
+    """ sends data from test function to a JSON object """
+
+    ssid = str.strip(ssid)
+
+    dictionary = {
+        "Start time": now,
+        "Network name": ssid,
+        "Down speed": down_speed,
+        "Up speed": up_speed,
+        "Ping": ping,
+        "Run time": runtime
+    }
+
+    filename = "speed.json"
+
+    json_object = json.dumps(dictionary, indent = 4)
+
+    with open(filename, "a", encoding='ascii') as outfile:
+        outfile.write(json_object + ",\n")
 
 def test():
     """ Performs the speedtest and prints download, upload speeds and ping """
-    st = speedtest.Speedtest()
+    speed = speedtest.Speedtest()
 
     servernames = []
-    st.get_servers(servernames)
+    speed.get_servers(servernames)
 
-    down_speed = st.download()
-    up_speed = st.upload()
-    ping = st.results.ping
+    down_speed = speed.download()
+    up_speed = speed.upload()
+    ping = speed.results.ping
 
-    # TODO: send to JSON object
     print("Download speed: " + str(int(down_speed)) + " b/s")
     print("Upload speed: " + str(int(up_speed)) + " b/s")
     print("Ping: " + str(int(ping)) + " ms")
+
+    return int(down_speed), int(up_speed), int(ping)
 
 def main():
     """ The main function """
     start = time.time()
     now = time.ctime(start)
+    ssid = os.popen("iwgetid -r").read()
 
-    print("Beginning speedtest: " + str(now))
+    print("Beginning speedtest of " + ssid + str(now))
 
-    test()
+    down_speed, up_speed, ping = test()
 
     end = time.time()
 
     print("Speedtest completed in " + str(int(end-start)) + " seconds")
+
+    print("Writing to JSON object...")
+
+    send_to_json(now, ssid, down_speed, up_speed, ping, int(end-start))
+
+    print("Writing complete")
+
 
 if __name__ == "__main__":
     main()
