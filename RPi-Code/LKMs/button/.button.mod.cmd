7c090e737d58336e87831a3d3c8e135a02705e40
IBRice101 <ibasquerice@gmail.com>
2023-01-14 16:41:17 +0000

    compiled the button file, probably best to commit here just in case this bloody makefile doesn't work again

---
+++ RPi-Code/LKMs/button/.button.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/pi/CMP408-Code/RPi-Code/LKMs/button/button.mod := { echo  /home/pi/CMP408-Code/RPi-Code/LKMs/button/button.o;  echo; } > /home/pi/CMP408-Code/RPi-Code/LKMs/button/button.mod
---
+++ RPi-Code/LKMs/button/button.mod
@@ -0,0 +1,2 @@
+/home/pi/CMP408-Code/RPi-Code/LKMs/button/button.o
+
