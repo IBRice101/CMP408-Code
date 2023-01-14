04617dbf1311adf559bebbd4ea2a887ce682ce1f
IBRice101 <ibasquerice@gmail.com>
2023-01-14 16:17:47 +0000

    removed those hidden files

--- RPi-Code/LKMs/button/.Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/pi/CMP408-Code/RPi-Code/LKMs/button/Module.symvers := sed 's/\.ko$$/\.o/' /home/pi/CMP408-Code/RPi-Code/LKMs/button/modules.order | scripts/mod/modpost -m -a  -o /home/pi/CMP408-Code/RPi-Code/LKMs/button/Module.symvers -e -i Module.symvers   -T -
--- RPi-Code/LKMs/button/.button.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/pi/CMP408-Code/RPi-Code/LKMs/button/button.mod := { echo  /home/pi/CMP408-Code/RPi-Code/LKMs/button/button.o;  echo; } > /home/pi/CMP408-Code/RPi-Code/LKMs/button/button.mod
