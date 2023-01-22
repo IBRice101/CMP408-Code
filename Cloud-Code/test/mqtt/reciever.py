57e1cad5688cdc9e66c4cb0d8979daedd23b4f71
Ubuntu <ubuntu@ip-172-31-82-85.ec2.internal>
2023-01-22 23:06:42 +0000

    fixed reciever

--- Cloud-Code/test/mqtt/reciever.py
+++ Cloud-Code/test/mqtt/reciever.py
@@ -1,4 +1,6 @@
 import paho.mqtt.client as mqtt
+import time
+import json
 
 # MQTT settings
 broker_address = "3.91.200.59"
@@ -20,11 +22,14 @@ client.subscribe(topic)
 
 # Define callback function for incoming messages
 def on_message(client, userdata, message):
-    print("Received message: ", message.payload)
+    # convert the received message (string) to json format
+    data = json.loads(message.payload)
+    # save the json data to a file named "data.json"
+    with open("data.json", "w") as f:
+        json.dump(data, f)
+    print("Data saved to disk")
 
 # Set callback function
 client.on_message = on_message
 
-# Keep script running to receive messages
 client.loop_forever()
-
