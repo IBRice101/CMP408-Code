e7448f2e3b2fd05fbeca1335b5376090691b037d
IBRice101 <ibasquerice@gmail.com>
2023-01-22 22:58:16 +0000

    added data.json sending functionality

--- Cloud-Code/test/mqtt/sender.py
+++ Cloud-Code/test/mqtt/sender.py
@@ -1,19 +1,30 @@
 import paho.mqtt.client as mqtt
+import json
 
 # MQTT settings
-broker_address = "35.173.187.104"
+broker_address = "3.91.200.59"
+username = "user"
+password = "password"
 topic = "testdata"
 
 # Create MQTT client
 client = mqtt.Client()
 
+# Set username and password as auth tokens
+client.username_pw_set(username, password)
+
 # Connect to broker
 client.connect(broker_address)
 
-# Send test data to server
-test_data = "This is a test message"
-client.publish(topic, test_data)
+# Read the JSON file
+with open("data.json", "r") as f:
+    data = json.load(f)
+
+# Convert the JSON data to a string
+data_str = json.dumps(data)
+
+# Send the JSON data to the server
+client.publish(topic, data_str)
 
 # Disconnect from broker
 client.disconnect()
-
